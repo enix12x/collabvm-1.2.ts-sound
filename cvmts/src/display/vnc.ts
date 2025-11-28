7fba78f74d54bc2eb64d0ad48692ecb3d391bb39
enix <enix12x@gmail.com>
2025-11-28 21:00:55 +0100

    Add files via upload

--- cvmts/src/display/vnc.ts
+++ cvmts/src/display/vnc.ts
@@ -1,4 +1,4 @@
-import { VncClient } from '@computernewb/nodejs-rfb';
+import { VncClient } from '@wize-logic/nodejs-rfb';
 import { EventEmitter } from 'node:events';
 import { Clamp } from '../Utilities.js';
 import { BatchRects } from './batch.js';
@@ -30,11 +30,13 @@ export class VncDisplay extends EventEmitter implements VMDisplay {
 		encodings: [
 			VncClient.consts.encodings.raw,
 
-			//VncClient.consts.encodings.pseudoQemuAudio,
+			VncClient.consts.encodings.pseudoQemuAudio,
 			VncClient.consts.encodings.pseudoDesktopSize
 			// For now?
 			//VncClient.consts.encodings.pseudoCursor
-		]
+		],
+		audioChannels: 2,
+		audioFrequency: 48000
 	});
 
 	private vncShouldReconnect: boolean = false;
@@ -91,6 +93,10 @@ export class VncDisplay extends EventEmitter implements VMDisplay {
 			rects = [];
 
 			this.emit('frame');
+		});
+
+		this.displayVnc.on('audioStream', (pcm: Buffer) => {
+			this.emit('audioStream', pcm);
 		});
 	}
 
